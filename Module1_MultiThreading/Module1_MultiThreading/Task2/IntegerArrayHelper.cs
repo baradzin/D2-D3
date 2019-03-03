6eeb94e7a2fb1e6e64be4ec5601362d5bbc8a9bc
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2019-03-03 20:13:26 +0300

    Task2 - complete

---
+++ Module1_MultiThreading/Module1_MultiThreading/Task2/IntegerArrayHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class IntegerArrayHelper
+    {
+        public Random Random { get; set; }
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+
+        public IntegerArrayHelper(int minValue, int maxValue)
+        {
+            this.Random = new Random();
+            this.MinValue = minValue;
+            this.MaxValue = maxValue;
+        }
+
+        public int[] CreateArray(int numberOfElements)
+        {
+            var arrayInts = new int[numberOfElements];
+            Console.WriteLine("Created random array:");
+            for (int i = 0; i < 10; i++) {
+                var number = this.Random.Next(MinValue, MaxValue);
+                arrayInts[i] = number;
+                Console.WriteLine(number);
+            }
+            Console.WriteLine();
+            return arrayInts;
+        }
+
+        public int[] MultiplyArray(int[] array)
+        {
+            int a = this.Random.Next(MinValue, MaxValue);
+            Console.WriteLine($"Multiplying scalar and vector \nScalar: {a} \nVector:");
+            for (int i = 0; i < array.Length; i++) {
+                array[i] = array[i] * a;
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine();
+            return array;
+        }
+
+        public int[] SortArrayAscending(int[] array)
+        {
+            Array.Sort(array);
+            Console.WriteLine("SortedList: ");
+            foreach (var ar in array) {
+                Console.WriteLine(ar);
+            }
+            Console.WriteLine();
+            return array;
+        }
+
+        public double AverageValue(int[] array)
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++) {
+                sum += array[i];
+            }
+            double averageValue = sum / array.Length;
+            Console.WriteLine($"Average value: {averageValue}");
+            return averageValue;
+        }
+    }
+}
---
+++ Module1_MultiThreading/Module1_MultiThreading/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        const int MIN_VALUE = 0;
+        const int MAX_VALUE = 20;
+        const int ARRAY_ELEMENTS_COUNT = 10;
+
+        static void Main(string[] args)
+        {
+            var helper = new IntegerArrayHelper(MIN_VALUE, MAX_VALUE);
+            Task.Factory.StartNew(() => helper.CreateArray(ARRAY_ELEMENTS_COUNT))
+                .ContinueWith((x) => helper.MultiplyArray(x.Result))
+                .ContinueWith((x) => helper.SortArrayAscending(x.Result))
+                .ContinueWith((x) => helper.AverageValue(x.Result)).Wait();
+            Console.ReadKey();
+        }
+    }
+}
