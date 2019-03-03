0268c17256f58d008f455dd01417c347647dc98d
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2019-03-03 21:19:39 +0300

    Task3 - Complete

---
+++ Module1_MultiThreading/Module1_MultiThreading/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix1 = new RectangleMatrix(5, 10);
+            matrix1.FillMatrixWithRandomValues(0, 20);
+            Console.WriteLine("Matrix A: ");
+            matrix1.Print();
+            var matrix2 = new RectangleMatrix(10, 5);
+            matrix2.FillMatrixWithRandomValues(0, 20);
+            Console.WriteLine("Matrix B: ");
+            matrix2.Print();
+            var matrix3 = matrix1 * matrix2;
+            Console.WriteLine("Matrix C: ");
+            matrix3.Print();
+        }
+    }
+}
---
+++ Module1_MultiThreading/Module1_MultiThreading/Task3/RectangleMatrix.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class RectangleMatrix
+    {
+        public int N { get; set; }
+        public int M { get; set; }
+        public int[][] Matrix { get; set; }
+
+        public RectangleMatrix(int n, int m)
+        {
+            this.N = n;
+            this.M = m;
+            this.Matrix = CreateMatrix(n, m);
+        }
+
+        private int[][] CreateMatrix(int n, int m)
+        {
+            int[][] result = new int[n][];
+            for (int i = 0; i < n; ++i)
+                result[i] = new int[m];
+            return result;
+        }
+
+        public void FillMatrixWithRandomValues(int minValue, int maxValue)
+        {
+            var random = new Random();
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    Matrix[i][j] = random.Next(minValue, maxValue);
+                }
+            }
+        }
+
+        public static RectangleMatrix operator *(RectangleMatrix m1, RectangleMatrix m2)
+        {
+            int aRows = m1.Matrix.Length;
+            int aCols = m1.Matrix[0].Length;
+            int bRows = m2.Matrix.Length;
+            int bCols = m2.Matrix[0].Length;
+            if (aCols != bRows)
+                throw new Exception("Number of columns matrix A not equal to number of rows Matrix B." +
+                    " Multiplication is impossible");
+
+            var result = new RectangleMatrix(aRows, bCols);
+
+            Parallel.For(0, aRows, i => {
+                for (int j = 0; j < bCols; ++j) {
+                    for (int k = 0; k < aCols; ++k) {
+                        result.Matrix[i][j] += m1.Matrix[i][k] * m2.Matrix[k][j];
+                    } 
+                }
+            });
+
+            return result;
+        }
+
+        public void Print()
+        {
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    Console.Write($"{Matrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
