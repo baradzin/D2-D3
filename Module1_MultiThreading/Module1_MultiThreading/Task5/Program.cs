a7a40ba2fb19b2c982b5034defbfcda304822955
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2019-03-04 23:07:14 +0300

    Task5 - init commit

---
+++ Module1_MultiThreading/Module1_MultiThreading/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Task4
+{
+    class Program
+    {
+        public static Semaphore semaphore = new Semaphore(10, 10);
+        static void Main(string[] args)
+        {
+            var state = new ThreadState();
+            Console.WriteLine("First Task starting...");
+            semaphore.WaitOne();
+            var task = ThreadPool.QueueUserWorkItem(state.CreateThread, state);
+            Console.WriteLine("First Task ending...");
+        }
+    }
+}
---
+++ Module1_MultiThreading/Module1_MultiThreading/Task5/ThreadState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Task4
+{
+    internal class ThreadState
+    {
+        public int _depth;
+        public int _value;
+
+        public ThreadState(int recursionDepth = 10, int value = 20)
+        {
+            this._value = value;
+            this._depth = recursionDepth;
+        }
+
+        public void CreateThread(object threadState)
+        {
+            Console.WriteLine("Taking Task2....");
+            Program.semaphore.WaitOne();
+            var state = (ThreadState)threadState;
+            if (state._depth > 0) {
+                state._value--;
+                state._depth--;
+                Console.WriteLine($"ThreadId: {Thread.CurrentThread.ManagedThreadId} value: {state._value}");
+                ThreadPool.QueueUserWorkItem(state.CreateThread, state);
+              //  Thread.Sleep(1000);
+                Console.WriteLine($"ThreadId: {Thread.CurrentThread.ManagedThreadId} value: {state._value} finished");
+            }
+            Console.WriteLine("Releasing Task2....");
+            Program.semaphore.Release();
+        }
+    }
+}
