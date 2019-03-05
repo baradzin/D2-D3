5868fa06b424059bb51726f9e356486c4eabec51
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2019-03-05 19:19:10 +0300

    Task6 - almost done

---
+++ Module1_MultiThreading/Module1_MultiThreading/Task6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int collectionLength = 1000;
+            int threadCount = 50;
+            string threadElapsedTime = RunThreads(collectionLength, threadCount);
+            string taskElapsedTime = RunTasks(collectionLength, threadCount);
+            Console.WriteLine(threadElapsedTime);
+            Console.WriteLine(taskElapsedTime);
+        }
+
+        static string RunThreads(int collectionLength, int threadCount)
+        {
+            var list = new ThreadSafeCollection(numberElements: collectionLength);
+            Stopwatch watch = new Stopwatch();
+            Thread[] threadList = new Thread[threadCount];
+            for (int i = 0; i < threadCount; i+=2) {
+                threadList[i] = new Thread(() => list.AddRandomInts(list));
+                threadList[i+1] = new Thread(() => list.PrintCollection(list));
+            }
+
+            watch.Start();
+            //Parallel.ForEach(threadList, (t) => { t.Start(); });
+            foreach (var thread in threadList) {
+                thread.Start();
+            }
+            foreach (var thread in threadList) {
+                thread.Join();
+            }
+            watch.Stop();
+
+            return $"Elapsed time for Threads: {watch.ElapsedMilliseconds}";
+        }
+
+        static string RunTasks(int collectionLength, int threadCount)
+        {
+            var list = new ThreadSafeCollection(numberElements: collectionLength);
+            Stopwatch watch = new Stopwatch();
+            Task[] tasksList = new Task[threadCount];
+            for (int i = 0; i < threadCount; i +=2) {
+                tasksList[i] = new Task(() => list.AddRandomInts(list));
+                tasksList[i+1] = new Task(() => list.PrintCollection(list));
+            }
+
+            watch.Start();
+            //Parallel.ForEach(tasksList, (t) => { t.Start(); });
+            foreach (var task in tasksList) {
+                task.Start();
+            }
+            Task.WaitAll(tasksList);
+            watch.Stop();
+
+            return $"Elapsed time for Tasks: {watch.ElapsedMilliseconds}";
+        }
+    }
+}
---
+++ Module1_MultiThreading/Module1_MultiThreading/Task6/ThreadSafeCollection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class ThreadSafeCollection
+    {
+        object lockObject = new object();
+        Random rand = new Random();
+        int maxValue;
+        int minValue;
+        int numberElements;
+        public List<int> List { get; set; }
+
+        public ThreadSafeCollection(int minValue = 0, int maxValue = 20, int numberElements = 10)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.numberElements = numberElements;
+            List = CreateSharedCollection();
+        }
+        public List<int> CreateSharedCollection()
+        {
+            var list = new List<int>();
+            for (int i = 0; i < numberElements; i++) {
+                list.Add(rand.Next(minValue, maxValue));
+            }
+
+            return list;
+        }
+
+        public void AddRandomInts(ThreadSafeCollection list)
+        {
+            lock (lockObject) {
+                for(int i = 0; i < 10; i++) {
+                    var value = rand.Next(minValue, maxValue);
+                    Console.WriteLine($"ThreadId: {Thread.CurrentThread.ManagedThreadId} adding: {value}");
+                    list.List.Add(value);
+                }               
+                Thread.Sleep(20);
+            }
+        }
+
+        public void PrintCollection(ThreadSafeCollection list)
+        {
+            lock (lockObject) {
+                foreach (var item in list.List) {
+                    Console.WriteLine($"ThreadId: {Thread.CurrentThread.ManagedThreadId} print: {item}");
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
