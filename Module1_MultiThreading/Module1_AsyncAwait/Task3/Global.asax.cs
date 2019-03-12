b82d615c3218788811f7aa732bb613080fbc0120
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2019-03-12 18:55:14 +0300

    Task4 - EF CRUD async complete

---
+++ Module1_MultiThreading/Module1_AsyncAwait/Task3/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Task3.DAL;
+
+namespace Task3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //using (var client = new UserContext()) {
+            //    client.Database.CreateIfNotExists();
+            //    client
+            //}
+        }
+    }
+}
