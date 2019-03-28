e573f4801d39aab6e9c52e187202541ce9fbd8fb
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2019-03-28 17:27:21 +0300

    Init commit for task1

---
+++ Module2_ExpressionsAndIQueryable/Module2_ExpressionsAndQuerable/Task1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("Task1.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9307b483-6a4c-47d5-ad9b-4ad4a3643989")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
