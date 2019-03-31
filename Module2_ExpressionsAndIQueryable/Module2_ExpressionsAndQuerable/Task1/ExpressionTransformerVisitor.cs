cc7610838baf989fb9edb081ece7df4aad570a10
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2019-04-01 01:08:51 +0300

    Task1 - complete

---
+++ Module2_ExpressionsAndIQueryable/Module2_ExpressionsAndQuerable/Task1/ExpressionTransformerVisitor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Task1
+{
+    public class ExpressionTransformerVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract) {
+                
+                var tuple = InitBinaryExpressionParams(node);
+                var param = tuple.Item1;
+                var constant = tuple.Item2;
+
+                if (param != null && constant != null && constant.Type == typeof(int) && (int)constant.Value == 1) {
+                    switch (node.NodeType) {
+                        case ExpressionType.Add:
+                            return Expression.Increment(param);
+                        case ExpressionType.Subtract:
+                            return Expression.Decrement(param);
+                    }
+                }
+            }
+
+            return base.VisitBinary(node);
+        }
+
+        private Tuple<ParameterExpression, ConstantExpression> InitBinaryExpressionParams(BinaryExpression node)
+        {
+            ParameterExpression param = null;
+            ConstantExpression constant = null;
+            if (node.Left.NodeType == ExpressionType.Parameter) {
+                param = (ParameterExpression)node.Left;
+            } else if (node.Left.NodeType == ExpressionType.Constant) {
+                constant = (ConstantExpression)node.Left;
+            }
+
+            if (node.Right.NodeType == ExpressionType.Parameter) {
+                param = (ParameterExpression)node.Right;
+            } 
+
+            if (node.Right.NodeType == ExpressionType.Constant) {
+                constant = (ConstantExpression)node.Right;
+            }
+
+            return Tuple.Create(param, constant);
+        }
+    }
+}
---
+++ Module2_ExpressionsAndIQueryable/Module2_ExpressionsAndQuerable/Task1/ParameterReplacerVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace Task1
+{
+    public class ParameterReplacerVisitor : ExpressionVisitor
+    {
+        public Dictionary<string, ConstantExpression> ParametersDictionary { get; set; }
+
+        public ParameterReplacerVisitor(Dictionary<string, ConstantExpression> dict)
+        {
+            ParametersDictionary = dict;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+            => ParametersDictionary.TryGetValue(node.Name, out var ce) ? (Expression)ce : node;
+
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            //var parameters = node.Parameters
+            //    .Where(p => !ParametersDictionary.ContainsKey(p.Name));
+
+            return Expression.Lambda(Visit(node.Body), node.Parameters);  // don't visit the parameters
+        }
+       
+    }
+}
